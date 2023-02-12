5eb2a4f1ef62b5a9274f474f86f22f4e84383e62
Bian-nca <bianca.ohs@gmail.com>
2023-02-12 13:57:44 -0300

    att tela de users

---
+++ src/main/java/Model/UserAccess.java
@@ -0,0 +1,18 @@
+package Model;
+
+public class UserAccess 
+{
+    String administrativo, operacional, gerencia;
+    
+    public void setAdministrativo(String administrativo) {
+        this.administrativo = administrativo;
+    }
+
+    public void setOperacional(String operacional) {
+        this.operacional = operacional;
+    }
+
+    public void setGerencia(String gerencia) {
+        this.gerencia = gerencia;
+    }
+}
